9c146e5a24381af2f1002f34f6720226a24693a4
MahiZhan1606 <t.mahes161197@gmail.com>
2023-08-05 15:26:07 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import Router from "./components/routercomponent/Router";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header/Header";
+import AddBook from "./Components/Books/AddBook";
+
+import Books from "./Components/Books/Books";
+import { EditBook } from "./Components/Books/EditBook";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import AddStudent from "./Components/Users/AddStudent";
+import AddMentor from "./Components/Users/AddMentor";
+import { EditStudent } from "./Components/Users/EditStudent";
+import Users from "./Components/Users/Users";
+import { EditMentor } from "./Components/Users/EditMentor";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
-    <div className="body">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div style={{color:"red", fontSize:40, textAlign:"center" }}>BOOK LIBRARY</div>
-            <Header />
-            <Router />
-          </div>
-        </div>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/addbook" element={<AddBook />} />
+        <Route path="editbook/:id" element={<EditBook />} />
+        <Route path="/user" element={<Users />} />
+        <Route path="/addStudent" element={<AddStudent />} />
+        <Route path="/addMentor" element={<AddMentor />} />
+        <Route path="editstudent/:id" element={<EditStudent />} />
+        <Route path="editmentor/:id" element={<EditMentor/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </div>
   );
 }
