822ac3827fef2e86829468a2aa5c3dc50e29d60d
MahiZhan1606 <t.mahes161197@gmail.com>
2023-08-05 13:01:04 +0530

    New App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Router from "./components/routercomponent/Router";
+import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div style={{color:"red", fontSize:40, textAlign:"center" }}>BOOK LIBRARY</div>
+            <Header />
+            <Router />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
